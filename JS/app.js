a84d6b87c5963a6ac07d1b0c5be7047434a08c1d
Leesvntos <christopherleesantos@gmail.com>
2022-04-12 17:40:26 -0500

    add start button function

--- JS/app.js
+++ JS/app.js
@@ -8,40 +8,49 @@ let result = 0
 let hitPosition
 let currentTime = 10
 let timerId = null
-    
-function randomSquare() {
-    squares.forEach(square => {
-        square.classList.remove('mole');
-    })
-    let randomSquare = squares[(Math.floor(Math.random() * 9))];
-    randomSquare.classList.add('mole');
-    hitPosition = randomSquare.id 
-}
-squares.forEach(square => {
-    square.addEventListener('mousedown', () => {
-        if (square.id == hitPosition) {
-            result++;
-            score.textContent = result;
-            hitPosition = null;
+  
+
+function startGame() {
+    startButton.addEventListener('mousedown', () => {
+        function randomSquare() {
+            squares.forEach(square => {
+                square.classList.remove('mole');
+            })
+            let randomSquare = squares[(Math.floor(Math.random() * 9))];
+            randomSquare.classList.add('mole');
+            hitPosition = randomSquare.id 
         }
+        squares.forEach(square => {
+            square.addEventListener('mousedown', () => {
+                if (square.id == hitPosition) {
+                    result++;
+                    score.textContent = result;
+                    hitPosition = null;
+                }
+            })
+        })
+        function moveMole() {
+            let timerId = null;
+            timerId = setInterval(randomSquare, 1000);
+        }
+        
+        moveMole();
+        
+        function countDown() {
+            currentTime--;
+            timeLeft.textContent = currentTime;
+        
+            if (currentTime == 0) {
+                clearInterval(countDownTimerId);
+                clearInterval(timerId);
+                alert('GAME OVER! Your score is ' + result);
+                window.location.reload();
+                
+            }
+        }
+        
+        let countDownTimerId = setInterval(countDown, 1000)
     })
-})
-function moveMole() {
-    let timerId = null;
-    timerId = setInterval(randomSquare, 1000);
 }
 
-moveMole();
-
-function countDown() {
-    currentTime--;
-    timeLeft.textContent = currentTime;
-
-    if (currentTime == 0) {
-        clearInterval(countDownTimerId);
-        clearInterval(timerId);
-        alert('GAME OVER! Your score is ' + result);
-    }
-}
-
-let countDownTimerId = setInterval(countDown, 1000)
+startGame();
